55a8a2a1a29959edf24423bb01ca626f01690366
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2020-12-27 18:32:33 -0600

    submitting category form to backend

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth/index";
 import { Link } from "react-router-dom";
+import { createCategory} from "./apiAdmin";
+
 
 const AddCategory = () => {
     const [name, setName] = useState("")
@@ -20,7 +22,15 @@ const AddCategory = () => {
         setError("")
         setSuccess(false)
         // then make the request to create the Category
-    }
+        createCategory(user._id, token, {name}).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setError("");
+                setSuccess(true);
+            }
+        });
+    };
 
     const newCategoryForm = () => (
         <form onSubmit={clickSubmit}>
@@ -32,19 +42,45 @@ const AddCategory = () => {
                     onChange={handleChange}
                     value={name}
                     autoFocus
+                    required
                 />
 
             </div>
+             
                 <button className="btn btn-outline-primary">
                    Create Category
                 </button>
         </form>
     );
 
+    const showSuccess = () => {
+        if (success) {
+            return <h3 className="text-success">{name} is created</h3>
+        }
+    }
+
+    const showError = () => {
+        if (error) {
+            return <h3 className="text-danger">Category should be unique</h3>
+        }
+    }
+
+    const goBack = () => (
+        <div className="mt-5">
+             <Link to="/admin/dashboard" className="text-warning">Back to Dashboard</Link>
+        </div>
+    );
+
     return (
-        <Layout title="Create a Category" description={`Hello ${name}`} >
+        <Layout title="Create a Category" description={`Hello ${user.name}`} >
+             
             <div className="row">
-                <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+                <div className="col-md-8 offset-md-2">
+                   {newCategoryForm()}
+                   {showSuccess()}
+                   {showError()}
+                   {goBack()}
+                </div>
             </div>
 
         </Layout>
--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import {  isAuthenticated } from "./index";
 
+
 const AdminRoute = ({ component: Component, ...rest }) => (
     <Route
         {...rest}
