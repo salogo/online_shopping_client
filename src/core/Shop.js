641902e74fedebfce88d9d293821d891569900b5
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-01-05 22:15:02 -0600

    RadioBox component to filter by prices

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -3,11 +3,15 @@ import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
-
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
 
 const Shop = () => {
+    const  [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: []}
+    })
     const [categories, setCategories] = useState([])
-    const [error, setError] = useState(false)
+    const [/*error,*/ setError] = useState(false)
 
     const init = () => {
         getCategories().then(data => {
@@ -21,7 +25,14 @@ const Shop = () => {
 
     useEffect(() => {
         init()
-    }, [])
+    }, []);
+
+    const handleFilters = (filters, filterBy) => {
+        //console.log("Shop Component:", filters, filterBy);
+        const newFilters = { ...myFilters};
+        newFilters.filters[filterBy] = filters;
+        setMyFilters(newFilters);
+    };
 
     return (
         <Layout
@@ -30,17 +41,25 @@ const Shop = () => {
             className="container-fluid"
         >
             <div className="row">
-               <div className="col-4">
-                <h4>Filter by categories</h4>
-                 <ul>  
-                   <Checkbox categories={categories} />
-                 </ul>
-               </div>
-               <div className="col-8">right</div>
+                <div className="col-4">
+                    <h4>Filter by categories</h4>
+                    <ul>
+                        <Checkbox categories={categories}
+                            handleFilters={filters => handleFilters(filters, "category")}
+                        />
+                    </ul>
+
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox prices={prices}
+                            handleFilters={filters => handleFilters(filters, "prices")}
+                        />
+                    </div>
+                </div>
+                <div className="col-8">{JSON.stringify(myFilters)}</div>
             </div>
         </Layout>
     )
 }
 
 export default Shop;
-
