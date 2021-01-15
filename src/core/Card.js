cd792b2911516e165f5cd167de1a4a2dd0389229
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-01-14 19:16:23 -0600

    redirect a single product to details and related products

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShowImage from "./ShowImage";
+import moment from "moment";
+
 
 const Cart = ({ product, showViewProductButton = true }) => {
 
@@ -16,19 +18,56 @@ const Cart = ({ product, showViewProductButton = true }) => {
 
     )
   }
+
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-warning mt-2 mb-2">
+        Add to Card
+      </button>
+    );
+  };
+
+  /*  
+   const showStock = quantity => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock </span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of Stock </span>
+    );
+  };
+  */
+
+  const showStock = quantity => {
+    return quantity > 0 ? (
+      <span  className="btn btn-success mt-2 mb-2">In Stock </span> 
+    ) : (
+      <span  className="btn btn-danger mt-2 mb-2">Out of Stock </span>  
+    );
+  };
+  
+
   return (
 
     <div className="card">
-      <div className="card-header">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
         <p>{product.description.substring(0, 100)}</p>
         <p>${product.price}</p>
+        <p >
+          Category: {product.category && product.category.name}
+        </p>
+        <p>
+          Added on {moment(product.createdAt).fromNow()}
+        </p>
+
+        {showStock(product.quantity)}
+        <br />
+
         {showViewButton(showViewProductButton)}
 
-        <button className="btn btn-outline-warning mt-2 mb-2">
-          Add to Card
-                  </button>
+        {showAddToCartButton()}
+
       </div>
     </div>
 
--- src/core/Product.js
+++ src/core/Product.js
@@ -1,45 +1,65 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { read } from "./apiCore";
+import { read, listRelated } from "./apiCore";
 import Card from "./Card";
 
 
 const Product = (props) => {
-    const [product, setProduct] =useState({})
-    const [error, setError] = useState(false)
+    const [product, setProduct] = useState({});
+    const [relatedProduct, setRelatedProduct] = useState([]);
+    const [error, setError] = useState(false);
 
     const loadSingleProduct = productId => {
-          read(productId).then(data => {
-              if(data.error) {
-                  setError(data.error);
-              } else {
-                  setProduct(data);
-              }
-          })
+        read(productId).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProduct(data);
+                // fetch related products
+                listRelated(data._id).then(data => {
+                    if (data.error) {
+                        setError(data.error)
+                    } else {
+                        setRelatedProduct(data);
+                    }
+                })
+            }
+        })
     }
 
     useEffect(() => {
         const productId = props.match.params.productId // from the URL
         loadSingleProduct(productId)
-    }, [])
+    }, [props])// [props] mean anytime there a change useEffect will update the state
 
 
     return (
-        <Layout 
-        title={product && product.name}
-         description={
-             product &&
-             product.description &&
-             product.description.substring(0, 100)}
-          className="container-fluid"
-          >
-         
-           <div className="col-4 mb-3">
-             {product && product.description && (
-                <Card product={product} showViewProductButton={false} />
-             )}
-           </div>
-                 
+        <Layout
+            title={product && product.name}
+            description={
+                product &&
+                product.description &&
+                product.description.substring(0, 100)}
+            className="container-fluid"
+        >
+
+            <div className="row">
+                <div className="col-8">
+                    {product && product.description && (
+                        <Card product={product} showViewProductButton={false} />
+                    )}
+                </div>
+                <div className="col-4">
+                    <h4>Related products</h4>
+                    {relatedProduct.map((p, i) => (
+                        <div className="mb-3">
+                            <Card key={i} product={p} />
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+
         </Layout>
     )
 }
