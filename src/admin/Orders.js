d831ab94fd80bb26c161cdf074ae0e677388a847
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-01-30 22:21:34 -0600

    update orders status

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -3,60 +3,163 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
-import { listOrders } from './apiAdmin';
+import { listOrders, getStatusValues } from './apiAdmin';
+import moment from "moment";
 
 const Orders = () => {
-   const [ orders, setOrders ] = useState([])
+    const [orders, setOrders] = useState([])
+    const [statusValues, setStatusValues ] = useState([]);
 
-   const { user, token } = isAuthenticated()
+    const { user, token } = isAuthenticated()
 
-   const loadOrders = () => {
-    listOrders(user._id, token).then(data => {
-        if (data.error) {
-            console.log(data.error);
+    const loadOrders = () => {
+        listOrders(user._id, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setOrders(data);
+            }
+        });
+    };
+
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setStatusValues(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadOrders();
+        loadStatusValues();
+    }, [])
+
+    const showOrdersLength = () => {
+        if (orders.length > 0) {
+            return (
+                <h1 className="text-danger display-2">
+                    Total orders: {orders.length}
+                </h1>
+            );
         } else {
-            setOrders(data);
+            return <h1 className="text-danger">No orders</h1>
         }
-    });
-};
+    };
 
-   useEffect(() => {
-       loadOrders()
-   }, [])
-
-   const noOrders = orders => {
-       return orders.length < 1 ? <h4>No orders</h4> : null;
-   };
- 
-
-   return (
-    <Layout title="Ordres" description={`Hello ${user.name}`} >
-         
-        <div className="row">
-            <div className="col-md-8 offset-md-2">
-            {noOrders(orders)}
-            {JSON.stringify(orders)}
-               
+    const showInput = (key, value) => (
+        <div className="input-group mb-2 mr-sm-2">
+            <div className="input-group-prepend">
+                <div className="input-group-text">{key}</div>
             </div>
+            <input
+                type="text"
+                value={value}
+                className="form-control"
+                readOnly
+            />
         </div>
+    )
 
-    </Layout>
-)
+    const goBack = () => (
+        <div className="mt-5">
+            <Link to="/admin/dashboard" className="text-warning">Back to Dashboard</Link>
+        </div>
+    );
+
+    const handleStatusChange = (e, orderId) => {
+        console.log("update order status")
+    }
+
+    const showStatus = (o) => (
+        <div className="form-group">
+           <h3 className="mark mb-4">Status: {o.status}</h3>
+           <select 
+              className="form-control" 
+              onChange={(e) => (handleStatusChange(e, o._id))}
+              >
+               <option>Update Status</option>
+                  {statusValues.map((status, index) => (
+                      <option key={index} value={status}>
+                         {status}
+                      </option>
+                  ))}
+           </select>
+        </div>
+    )
 
 
+    return (
+        <Layout title="Ordres" description={`Hello ${user.name}`} >
+
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    {showOrdersLength()}
+
+                    {orders.map((o, oindex) => {
+                        return (
+                            <div
+                                className="mt-5"
+                                key={oindex}
+                                style={{ borderBottom: "5px solid indigo" }}>
+                                <h2 className="mb-5">
+                                    <span className="bg-secondary">
+                                        Order ID: {o._id}
+                                    </span>
+                                </h2>
+                                <ul className="list-group mb-2">
+                                    <li className="list-group-item">
+                                        {showStatus(o)}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Transaction ID:  {o.transaction_id}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Amount: ${o.amount}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Ordered by: {o.user.name}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Ordered on: {moment(o.createdAt).fromNow()}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Delivery address: {o.address}
+                                    </li>
+                                </ul>
+                                <h3 className="mt-4 mb-4 font-italic">
+                                    Total products in the order: {o.products.length}
+                                </h3>
+
+                                {o.products.map((p, pIndex) => (
+                                    <div
+                                        className="mb-4"
+                                        key={pIndex}
+                                        style={{
+                                            padding: "20px",
+                                            border: "1px solid indigo"
+                                        }}
+                                    >
+                                        {showInput("Product name", p.name)}
+                                        {showInput("Product price", p.price)}
+                                        {showInput("Product total", p.count)}
+                                        {showInput("Product Id", p._id)}
+                                    </div>
+                                ))}
+                            </div>
+                        );
+                    })}
+
+                    {goBack()}
+
+                </div>
+            </div>
+
+        </Layout>
+    )
 
 }
 
 export default Orders;
-
-
-/*
-import React from "react";
-
-const Orders = () => <div>Hello from orders route</div>;
-
-
-
-
-export default Orders;
-*/
