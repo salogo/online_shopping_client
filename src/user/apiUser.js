f3b80032497767342546e6582db41fdc4ee8e7aa
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-02-02 05:03:46 -0600

    user purchase histories

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -43,3 +43,18 @@ export const read = (userId, token) => {
           }
       }
   }
+
+  export const getPurchaseHistory = (userId, token) => {
+    return fetch(`${API}/orders/by/user/${userId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+    }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+  };
